a065913876a086a74cb0f30bf7fe3bfc693f58ed
Oliver Nguyen <olivernguyen@google.com>
2020-10-08 12:08:40 -0700

    Reland "Enable memory-mapped coverage after forking."
    This prevents opening files before the fork, and failing system_server
    checks when forking related to open file descriptors.
    
    Test: m EMMA_INSTRUMENT=true EMMA_INSTRUMENT_FRAMEWORK=true and verify
    /data/misc/trace/jacoco-*.mm.ec files
    Bug: 169426258
    
    Change-Id: Id7e6825bb70f848a37af6907125af7cdc5fcaf50
    Merged-In: Id7e6825bb70f848a37af6907125af7cdc5fcaf50

--- dalvik/src/main/java/dalvik/system/ZygoteHooks.java
+++ dalvik/src/main/java/dalvik/system/ZygoteHooks.java
@@ -21,6 +21,11 @@ import android.icu.text.DecimalFormatSymbols;
 import android.icu.util.ULocale;
 
 import java.io.File;
+import java.io.FileDescriptor;
+import java.lang.reflect.Method;
+import java.lang.ClassNotFoundException;
+import java.lang.NoSuchMethodException;
+import java.lang.ReflectiveOperationException;
 
 /**
  * Provides hooks for the zygote to call back into the runtime to perform
@@ -31,6 +36,7 @@ import java.io.File;
 @libcore.api.CorePlatformApi
 public final class ZygoteHooks {
     private static long token;
+    private static Method enableMemoryMappedDataMethod;
 
     /** All methods are static, no need to instantiate. */
     private ZygoteHooks() {
@@ -57,6 +63,19 @@ public final class ZygoteHooks {
         ULocale[] localesToPin = { ULocale.ROOT, ULocale.US, ULocale.getDefault() };
         for (ULocale uLocale : localesToPin) {
             new DecimalFormatSymbols(uLocale);
+        }
+
+        // Look up JaCoCo on the boot classpath, if it exists. This will be used later for enabling
+        // memory-mapped Java coverage.
+        try {
+          Class<?> jacocoOfflineClass = Class.forName("org.jacoco.agent.rt.internal.Offline");
+          enableMemoryMappedDataMethod = jacocoOfflineClass.getMethod("enableMemoryMappedData");
+        } catch (ClassNotFoundException e) {
+          // JaCoCo was not on the boot classpath, so this is not a coverage build.
+        } catch (NoSuchMethodException e) {
+          // Method was not found in the JaCoCo Offline class. The version of JaCoCo is not
+          // compatible with memory-mapped coverage.
+          throw new RuntimeException(e);
         }
     }
 
@@ -127,6 +146,16 @@ public final class ZygoteHooks {
         nativePostForkChild(token, runtimeFlags, isSystemServer, isZygote, instructionSet);
 
         Math.setRandomSeedInternal(System.currentTimeMillis());
+
+        // Enable memory-mapped coverage if JaCoCo is in the boot classpath. system_server is
+        // skipped due to being persistent and having its own coverage writing mechanism.
+        if (!isSystemServer && enableMemoryMappedDataMethod != null) {
+          try {
+            enableMemoryMappedDataMethod.invoke(null);
+          } catch (ReflectiveOperationException e) {
+            throw new RuntimeException(e);
+          }
+        }
     }
 
     /**
